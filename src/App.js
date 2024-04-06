d869ed3c631f0937ac0f41ea0ce5eb9547559b85
zydnwz <zaidnawaz@2018@gmail.com>
2024-04-06 14:45:43 +0530

    Making Api calls in React

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/layout/Footer";
-import CartProvider from "./components/Store/CartProvider";
-import Routers from "./components/routers/Routers";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+
+    setMovies(transformedMovies);
+  }
+
   return (
-    <Router>
-      <CartProvider>
-        <Header></Header>
-        <Routers></Routers>
-        <Footer></Footer>
-      </CartProvider>
-    </Router>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
   );
 }
 
