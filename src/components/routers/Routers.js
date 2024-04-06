a691b5ffeff18f0484963bbd9f732cf953b06b4c
zydnwz <zaidnawaz@2018@gmail.com>
2024-04-06 14:02:25 +0530

    Implement the Home page

--- src/components/routers/Routers.js
+++ src/components/routers/Routers.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductList from "../products/ProductList";
 import About from "../layout/About";
+import Home from "../layout/Home";
 import { Route, Routes } from "react-router-dom";
 
 const routePath = {
@@ -10,13 +11,15 @@ const routePath = {
 };
 
 const Routers = () => {
-  return <div>
-    <Routes>
-        <Route path ={routePath.Home} element={<ProductList />} />
-        <Route path ={routePath.Store} element={<ProductList />} />
-        <Route path ={routePath.About} element={<About />} />
-    </Routes>
-  </div>;
+  return (
+    <div>
+      <Routes>
+        <Route path={routePath.Home} element={<Home />} />
+        <Route path={routePath.Store} element={<ProductList />} />
+        <Route path={routePath.About} element={<About />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Routers;
