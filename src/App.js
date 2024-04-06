7816bc8cdb3f34971bb061fab8909278d952a889
zydnwz <zaidnawaz@2018@gmail.com>
2024-04-06 23:26:24 +0530

    Adding Contact US Page via REACT ROUTER

--- src/App.js
+++ src/App.js
@@ -1,117 +1,20 @@
-import React, { useState, useEffect, useCallback } from "react";
-import AddMovie from "./components/AddMovie";
-import MoviesList from "./components/MoviesList";
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/layout/Footer";
+import CartProvider from "./components/Store/CartProvider";
+import Routers from "./components/routers/Routers";
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const fetchMoviesHandler = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(
-        "https://react-http-88ca9-default-rtdb.firebaseio.com/movies.json"
-      );
-      if (!response.ok) {
-        throw new Error("Something went wrong!");
-      }
-
-      const data = await response.json();
-
-      const loadedMovies = [];
-
-      for (const key in data) {
-        loadedMovies.push({
-          id: key,
-          title: data[key].title,
-          openingText: data[key].openingText,
-          releaseDate: data[key].releaseDate,
-        });
-      }
-
-      setMovies(loadedMovies);
-    } catch (error) {
-      setError(error.message);
-    }
-    setIsLoading(false);
-  }, []);
-
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
-
-  async function addMovieHandler(movie) {
-    try {
-      const response = await fetch(
-        "https://react-http-88ca9-default-rtdb.firebaseio.com/movies.json",
-        {
-          method: "POST",
-          body: JSON.stringify(movie),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Failed to add movie.");
-      }
-      const data = await response.json();
-      setMovies((prevMovies) => [
-        ...prevMovies,
-        { id: data.name, ...movie },
-      ]);
-    } catch (error) {
-      setError(error.message);
-    }
-  }
-
-  async function deleteMovieHandler(id) {
-    try {
-      const response = await fetch(
-        `https://react-http-88ca9-default-rtdb.firebaseio.com/movies/${id}.json`,
-        {
-          method: "DELETE",
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Failed to delete movie.");
-      }
-      setMovies((prevMovies) =>
-        prevMovies.filter((movie) => movie.id !== id)
-      );
-    } catch (error) {
-      setError(error.message);
-    }
-  }
-
-  let content = <p>Found no movies.</p>;
-
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} onDeleteMovie={deleteMovieHandler} />;
-  }
-
-  if (error) {
-    content = <p>{error}</p>;
-  }
-
-  if (isLoading) {
-    content = <p>Loading...</p>;
-  }
-
   return (
-    <React.Fragment>
-      <section>
-        <AddMovie onAddMovie={addMovieHandler} />
-      </section>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>{content}</section>
-    </React.Fragment>
+    <Router>
+      <CartProvider>
+        <Header></Header>
+        <Routers></Routers>
+        <Footer></Footer>
+      </CartProvider>
+    </Router>
   );
 }
 
