93a7fc973f857767381c625daad4e96daaf1b1e4
zydnwz <zaidnawaz@2018@gmail.com>
2024-04-07 04:35:37 +0530

    Auto logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   token: "",
@@ -22,6 +22,19 @@ export const AuthContextProvider = (props) => {
     setToken(null);
     localStorage.removeItem("token");
   };
+
+  useEffect(() => {
+    let logoutTimer;
+
+    if (userIsLoggedIn) {
+      logoutTimer = setTimeout(() => {
+        logoutHandler();
+        alert("You have been logged out due to inactivity.");
+      }, 5 * 60 * 1000);
+    }
+
+    return () => clearTimeout(logoutTimer);
+  }, [userIsLoggedIn]);
 
   const contextValue = {
     token: token,
